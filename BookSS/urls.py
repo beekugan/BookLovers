66605573fdf664e06c1c8a0c7449aa8f7d47a807
TarasykBez <tarasbezushko23@gmail.com>
2025-03-04 15:17:05 +0200

    -Створено процес реєстрації для типу користувача librarian -додано шаблон home для показу профілю користувача -додані шляхи для login, logout

--- BookSS/urls.py
+++ BookSS/urls.py
@@ -1,22 +1,8 @@
-"""
-URL configuration for BookSS project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-from users.views import register,  register_reader, get_specialities
+from users.views import (register, register_reader, get_specialities, register_librarian,
+                         confirm_email, home, login_view, logout_view)
+
 from books.views import add_genre
 
 urlpatterns = [
@@ -24,8 +10,11 @@ urlpatterns = [
     path("add-genre/", add_genre, name="add_genre"),
     path("register/", register, name="register"),
     path("register_reader/<str:email>/", register_reader, name="register_reader"),
-
+    path("register_librarian/<str:email>/", register_librarian, name="register_librarian"),
+    path("confirm_email/<str:uid>/", confirm_email, name="confirm_email"),
     path("get_specialities/", get_specialities, name="get_specialities"),
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
 
-
+    path("home/", home, name="home"),
 ]
